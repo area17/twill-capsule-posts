afee65374c18c75a087b4d7144e9921c6fd4c1c6
Quentin Renard <quentin@area17.com>
2021-05-10 12:06:17 +0200

    Enable reordering

--- Http/Controllers/PostController.php
+++ Http/Controllers/PostController.php
@@ -2,14 +2,18 @@
 
 namespace App\Twill\Capsules\Posts\Http\Controllers;
 
+use A17\Twill\Http\Controllers\Admin\ModuleController;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use A17\Twill\Http\Controllers\Admin\ModuleController;
 
 class PostController extends ModuleController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     protected $moduleName = 'posts';
+
+    protected $indexOptions = [
+        'reorder' => true,
+    ];
 }
