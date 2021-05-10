c45d04dd4e2b9e5440a81ad652468c7912c82780
Quentin Renard <quentin@area17.com>
2021-05-10 11:58:36 +0200

    Update create form

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,13 +1,24 @@
 @formField('input', [
-    'name' => 'description',
-    'label' => 'Description',
+    'name' => 'title',
+    'label' => 'Title',
     'translated' => true,
-    'maxlength' => 100
+    'maxlength' => 100,
+    'onChange' => 'formatPermalink'
 ])
 
+@unless(isset($item))
+    @formField('input', [
+        'name' => 'description',
+        'label' => 'Description',
+        'translated' => true,
+        'maxlength' => 100
+    ])
+@endunless
+
 @formField('input', [
-    'name' => 'description',
-    'label' => 'Description',
+    'name' => 'slug',
+    'label' => 'Permalink',
     'translated' => true,
-    'maxlength' => 100
+    'ref' => 'permalink',
+    'prefix' => $permalinkPrefix ?? ''
 ])
